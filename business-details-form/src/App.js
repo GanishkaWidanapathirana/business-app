fdf10a35a4f30788f7391cb19c77827eada74ac2
GanishkaHWC <79002523+GanishkaWidanapathirana@users.noreply.github.com>
2024-09-08 13:42:43 +0530

    2nd commit

--- business-details-form/src/App.js
+++ business-details-form/src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const serviceURL = window?.configs?.apiUrl ? window.configs.apiUrl : "/";
+
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log(serviceURL)
+        const response = await axios.get(`${serviceURL}/your-endpoint`);
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
